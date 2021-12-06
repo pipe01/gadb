a6f4878cef5763936b3df49cd7dc751e7e7becc3
Felipe Martinez <pipernene@gmail.com>
2021-12-06 17:34:55 +0100

    Change author

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/electricbubble/gadb"
+	"github.com/pipe01/gadb"
 	"log"
 	"os"
 	"strings"
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/electricbubble/gadb
+module github.com/pipe01/gadb
 
 go 1.15
